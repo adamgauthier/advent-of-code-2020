235dedb942eeb6e29949a7e98b1eeeda25e03b2b
Adam Gauthier <adamgauthier12@gmail.com>
2020-12-05 16:41:46 -0500

    Add solution for day 01 part two

--- 01-report-repair/src/Program.fs
+++ 01-report-repair/src/Program.fs
@@ -1,22 +1,43 @@
 open System.IO
 
+let GetProduct numbers =
+    Seq.fold (*) 1 numbers
+
+let SumsTo sum (numbers: seq<int>) =
+    Seq.sum numbers = sum
+
 let rec GetAllPairs l =
     seq {
         match l with
         | head :: tail ->
             for element in tail do
-                yield (head, element)
+                yield [head; element]
             yield! GetAllPairs tail
         | _ -> ()
     }
 
-let FindPairThatSumsTo sumTo numbers =
+let SolvePuzzle numbers =
     GetAllPairs numbers
-    |> Seq.find (fun (firstNumber, secondNumber) -> firstNumber + secondNumber = sumTo)
+    |> Seq.find (SumsTo 2020)
+    |> GetProduct
 
-let SolvePuzzle numbers = 
-    let (a, b) = FindPairThatSumsTo 2020 numbers
-    (a * b)
+
+let rec GetAllTriplets numbers =
+    seq {
+        match numbers with
+        | head :: tail ->
+            let tailPairs = GetAllPairs tail
+            for pair in tailPairs do
+                yield head :: pair
+            yield! GetAllTriplets tail
+        | _ -> ()
+    }
+
+let SolvePuzzlePartTwo numbers =
+    GetAllTriplets numbers
+    |> Seq.find (SumsTo 2020)
+    |> GetProduct
+
 
 [<EntryPoint>]
 let main argv =
@@ -26,7 +47,11 @@ let main argv =
         |> Seq.map int
         |> Seq.toList
 
-    let answer = SolvePuzzle numbers
+    let answer =
+        match Array.tryItem 1 argv with
+        | Some "2" ->  SolvePuzzlePartTwo numbers
+        | _ ->  SolvePuzzle numbers
+
     printfn "Answer is %d" answer
 
     0
--- 01-report-repair/tests/Tests.fs
+++ 01-report-repair/tests/Tests.fs
@@ -6,3 +6,8 @@ open Xunit
 let ``SolvePuzzle WhenExample ThenReturns514579`` () =
     let result = Program.SolvePuzzle [1721; 979; 366; 299; 675; 1456]
     Assert.Equal(result, 514579)
+
+[<Fact>]
+let ``SolvePuzzlePartTwo WhenExample ThenReturns241861950`` () =
+    let result = Program.SolvePuzzlePartTwo [1721; 979; 366; 299; 675; 1456]
+    Assert.Equal(result, 241861950)
