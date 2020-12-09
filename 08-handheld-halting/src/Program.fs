75be67b591ea4cc3278c43c50f4c4fb59e4d7cb2
Adam Gauthier <adamgauthier12@gmail.com>
2020-12-08 19:20:56 -0500

    Add solution for day 08 part two

--- 08-handheld-halting/src/Program.fs
+++ 08-handheld-halting/src/Program.fs
@@ -35,6 +35,50 @@ let SolvePuzzle rawInstructions =
 
     instructions |> (ExecuteUntilLoop 0 0)
 
+type ExecuteUntilEndResult =
+| RanToCompletion of int
+| LoopDetected
+
+let rec ExecuteUntilEnd i acc (wasSwapped: bool) (allInstructions: Map<int, Instruction>) =
+    match allInstructions |> Map.tryFind i with
+    | None -> RanToCompletion acc
+    | Some current ->
+        if current.Visited then
+            LoopDetected
+        else
+            let instructionsWithCurrentVisited =
+                allInstructions
+                |> Map.change i (fun (Some instruction) -> Some { instruction with Visited = true })
+
+            let executeNop wasSwapped = instructionsWithCurrentVisited |> ExecuteUntilEnd (i+1) acc wasSwapped
+            let executeJmp wasSwapped = instructionsWithCurrentVisited |> ExecuteUntilEnd (i+current.Argument) acc wasSwapped
+
+            match current.Operation with
+            | "acc" -> (instructionsWithCurrentVisited |> ExecuteUntilEnd (i+1) (acc + current.Argument) wasSwapped)
+            | "nop" ->
+                match executeNop wasSwapped with
+                | RanToCompletion result -> RanToCompletion result
+                | LoopDetected ->
+                    match wasSwapped with
+                    | true -> LoopDetected
+                    | false -> executeJmp true
+            | "jmp" ->
+                match executeJmp wasSwapped with
+                | RanToCompletion result -> RanToCompletion result
+                | LoopDetected ->
+                    match wasSwapped with
+                    | true -> LoopDetected
+                    | false -> executeNop true
+
+let SolvePuzzlePartTwo rawInstructions =
+    let instructions =
+        rawInstructions
+        |> List.map ParseInstruction
+        |> MakeMapWithIndexAsKey
+
+    let (RanToCompletion result) = instructions |> (ExecuteUntilEnd 0 0 false)
+    result
+
 [<EntryPoint>]
 let main argv =
 
@@ -45,5 +89,6 @@ let main argv =
         |> List.ofArray
 
     printfn "Answer for part one is %d" (SolvePuzzle lines)
+    printfn "Answer for part two is %d" (SolvePuzzlePartTwo lines)
 
     0
--- 08-handheld-halting/tests/Tests.fs
+++ 08-handheld-halting/tests/Tests.fs
@@ -16,3 +16,18 @@ jmp -4
 acc +6".Replace("\r\n", "\n").Split("\n") |> List.ofSeq)
 
     Assert.Equal(5, result)
+
+[<Fact>]
+let ``SolvePuzzlePartTwo WhenExample ThenReturns8`` () =
+    let result =
+        Program.SolvePuzzlePartTwo ("nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6".Replace("\r\n", "\n").Split("\n") |> List.ofSeq)
+
+    Assert.Equal(8, result)
