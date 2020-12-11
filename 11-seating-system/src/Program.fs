1e7f4d9324596eb51f8085c485d7d58d3af789c7
Adam Gauthier <adamgauthier12@gmail.com>
2020-12-11 01:42:16 -0500

    Add solution for day 11 part two

--- 11-seating-system/src/Program.fs
+++ 11-seating-system/src/Program.fs
@@ -51,23 +51,59 @@ let ApplyRules (chart: Chart): Chart =
             match seat with
             | Empty ->
                 match adjacents |> Seq.contains (Some Occupied) with
-                | true -> seat
+                | true -> Empty
                 | false -> Occupied
             | Occupied ->
                 match adjacents |> Seq.filter ((=) (Some Occupied)) |> Seq.length with
                 | occupied when occupied >= 4 -> Empty
-                | _ -> seat
+                | _ -> Occupied
             | Floor -> Floor
         )
     )
 
-let rec ApplyRulesUntilStability chart =
-    let newChart = ApplyRules chart
+let ApplyRulesPartTwo (chart: Chart): Chart =
+    chart
+    |> List.indexed
+    |> List.map (fun (lineIndex, line) ->
+        line
+        |> List.indexed
+        |> List.map (fun (columnIndex, seat) ->
+            let rec findFirstSeat lineI columnI transform =
+                let (lineToCheck, columnToCheck) = transform (lineI, columnI)
 
-    if newChart = chart then
-        newChart
-    else
-        ApplyRulesUntilStability newChart
+                let seatToCheck = GetSeat chart (lineToCheck, columnToCheck)
+
+                match seatToCheck with
+                | Some s when s <> Floor -> Some s
+                | Some _ -> findFirstSeat lineToCheck columnToCheck transform
+                | None -> None
+
+            let left = findFirstSeat lineIndex columnIndex (fun (lineI, columnI) -> (lineI, columnI - 1))
+            let right = findFirstSeat lineIndex columnIndex (fun (lineI, columnI) -> (lineI, columnI + 1))
+
+            let up = findFirstSeat lineIndex columnIndex (fun (lineI, columnI) -> (lineI - 1, columnI))
+            let upLeft = findFirstSeat lineIndex columnIndex (fun (lineI, columnI) -> (lineI - 1, columnI - 1))
+            let upRight = findFirstSeat lineIndex columnIndex (fun (lineI, columnI) -> (lineI - 1, columnI + 1))
+
+            let down = findFirstSeat lineIndex columnIndex (fun (lineI, columnI) -> (lineI + 1, columnI))
+            let downLeft = findFirstSeat lineIndex columnIndex (fun (lineI, columnI) -> (lineI + 1, columnI - 1))
+            let downRight = findFirstSeat lineIndex columnIndex (fun (lineI, columnI) -> (lineI + 1, columnI + 1))
+
+            let adjacents = [left; right; up; down; upLeft; upRight; downLeft; downRight]
+
+            match seat with
+            | Empty ->
+                match adjacents |> Seq.contains (Some Occupied) with
+                | true -> Empty
+                | false -> Occupied
+            | Occupied ->
+                match adjacents |> Seq.filter ((=) (Some Occupied)) |> Seq.length with
+                | occupied when occupied >= 5 -> Empty
+                | _ -> Occupied
+            | Floor -> Floor
+        )
+    )
+
 
 let CountSeats (chart: Chart) (seat: Seat) =
     chart |> List.sumBy (fun line ->
@@ -76,12 +112,21 @@ let CountSeats (chart: Chart) (seat: Seat) =
         |> List.length
     )
 
+let rec CountOccupiedSeatsAfterStability applyRules chart =
+    let newChart = applyRules chart
+
+    if newChart = chart then
+        CountSeats newChart Occupied
+    else
+        CountOccupiedSeatsAfterStability applyRules newChart
+
 let SolvePuzzle lines =
     let chart = ParseSeatingChart lines
-    let stabilizedChart = ApplyRulesUntilStability chart
+    CountOccupiedSeatsAfterStability ApplyRules chart
 
-    CountSeats stabilizedChart Occupied
-
+let SolvePuzzlePartTwo lines =
+    let chart = ParseSeatingChart lines
+    CountOccupiedSeatsAfterStability ApplyRulesPartTwo chart
 
 [<EntryPoint>]
 let main argv =
@@ -93,5 +138,6 @@ let main argv =
         |> List.ofArray
 
     printfn "Answer for part one is %d" (SolvePuzzle lines)
+    printfn "Answer for part two is %d" (SolvePuzzlePartTwo lines)
 
     0
--- 11-seating-system/tests/Tests.fs
+++ 11-seating-system/tests/Tests.fs
@@ -5,7 +5,7 @@ open Xunit
 [<Fact>]
 let ``SolvePuzzle WhenExample ThenReturns37`` () =
     let result =
-        Program.SolvePuzzle ("L.LL.LL.LL
+        Program.SolvePuzzle("L.LL.LL.LL
 LLLLLLL.LL
 L.L.L..L..
 LLLL.LL.LL
@@ -17,3 +17,19 @@ L.LLLLLL.L
 L.LLLLL.LL".Replace("\r\n", "\n").Split("\n") |> List.ofSeq)
 
     Assert.Equal(37, result)
+
+[<Fact>]
+let ``SolvePuzzlePartTwo WhenExample ThenReturns26`` () =
+    let result =
+        Program.SolvePuzzlePartTwo("L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL".Replace("\r\n", "\n").Split("\n") |> List.ofSeq)
+
+    Assert.Equal(26, result)
