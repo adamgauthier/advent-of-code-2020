d1bb92eb9ec0d4a202700730469c0b9c759f2bc8
Adam Gauthier <adamgauthier12@gmail.com>
2020-12-11 01:42:16 -0500

    Add solution for day 13 part two

--- 13-shuttle-search/src/Program.fs
+++ 13-shuttle-search/src/Program.fs
@@ -20,6 +20,46 @@ let SolvePuzzle lines =
     busId * (timestamp - earliest)
 
 
+let rec GreatestCommonDenominator x y = if y = 0L then abs x else GreatestCommonDenominator y (x % y)
+
+let LeastCommonMultiple x y = x * y / (GreatestCommonDenominator x y)
+
+let FindEarliestTimestampSuccessiveDepartures step busIds =
+    let rec findEarliestTimestampSuccessiveDepartures currentTimestamp (busIds: (int64 * int64) array) =
+        let timestampToTest = (currentTimestamp - (snd busIds.[0]))
+        let allMatches =
+            busIds
+            |> Array.forall (fun (i, busId) -> (timestampToTest + i) % busId = 0L)
+
+        if allMatches then
+            timestampToTest
+        else
+            findEarliestTimestampSuccessiveDepartures (currentTimestamp + step) busIds
+
+    findEarliestTimestampSuccessiveDepartures 0L busIds
+
+let SolvePuzzlePartTwo (lines: string list) =
+    let busIds =
+        lines.[1].Split(",")
+        |> Array.indexed
+        |> Array.filter (snd >> (<>) "x")
+        |> Array.map (fun (i, id) -> (int64 i, int64 id))
+
+    let busIdWithOffsetOfFirstBusId = busIds |> Array.find (fst >> (=) (snd busIds.[0]))
+
+    let busIdsWithPerfectOffsetToTarget = busIds |> Array.filter (fun (i, id) ->
+        let offsetToTarget = i - (fst busIdWithOffsetOfFirstBusId)
+        abs offsetToTarget = id
+    )
+
+    let leastCommonMultiple =
+        Array.append [| busIdWithOffsetOfFirstBusId |] busIdsWithPerfectOffsetToTarget
+        |> Array.map snd
+        |> Array.reduce LeastCommonMultiple
+
+    FindEarliestTimestampSuccessiveDepartures leastCommonMultiple busIds
+
+
 [<EntryPoint>]
 let main argv =
 
@@ -30,5 +70,6 @@ let main argv =
         |> List.ofArray
 
     printfn "Answer for part one is %d" (SolvePuzzle lines)
+    printfn "Answer for part two is %d" (SolvePuzzlePartTwo lines)
 
     0
--- 13-shuttle-search/tests/Tests.fs
+++ 13-shuttle-search/tests/Tests.fs
@@ -9,3 +9,11 @@ let ``SolvePuzzle WhenExample ThenReturns295`` () =
 7,13,x,x,59,x,31,19".Replace("\r\n", "\n").Split("\n") |> List.ofSeq)
 
     Assert.Equal(295L, result)
+
+[<Fact>]
+let ``SolvePuzzlePartTwo WhenFirstExample ThenReturns1068781`` () =
+    let result =
+        Program.SolvePuzzlePartTwo("939
+7,13,x,x,59,x,31,19".Replace("\r\n", "\n").Split("\n") |> List.ofSeq)
+
+    Assert.Equal(1068781L, result)
