3e0f5bb4dbe75b00b7d97393c94949c49e7f00d2
Adam Gauthier <adamgauthier12@gmail.com>
2020-12-12 22:52:18 -0500

    Add solution for day 10 part two

--- 10-adapter-array/src/Program.fs
+++ 10-adapter-array/src/Program.fs
@@ -1,7 +1,15 @@
 open System.IO
 
-let GetJoltDifferences (adapters: seq<int64>) outlet device =
-    let rec buildPathAndGetJoltDifferences (adapters: seq<int64>) currentJolts device differences =
+type PuzzleInput = { Adapters: seq<int64>; Device:int64; Outlet:int64 }
+
+let ParseInput input =
+    let adapters = input |> Seq.map int64 |> Seq.sort
+    let device = adapters |> Seq.max |> (+) 3L
+
+    { Adapters=adapters; Device=device; Outlet=0L }
+
+let GetJoltDifferences puzzleInput =
+    let rec buildPathAndGetJoltDifferences adapters currentJolts device differences =
         match device - currentJolts with
         | differenceWithDevice when differenceWithDevice <= 3L ->
             List.append differences [differenceWithDevice]
@@ -14,19 +22,49 @@ let GetJoltDifferences (adapters: seq<int64>) outlet device =
             let newDifferences = List.append differences [(nextAdapter - currentJolts)]
             buildPathAndGetJoltDifferences adapters nextAdapter device newDifferences
 
-    buildPathAndGetJoltDifferences adapters outlet device []
+    buildPathAndGetJoltDifferences puzzleInput.Adapters puzzleInput.Outlet puzzleInput.Device []
 
 let SolvePuzzle input =
-    let adapters = input |> Seq.map int64 |> Seq.sort
-    let outlet = 0L
-    let device = (Seq.max adapters) + 3L
+    let puzzleInput = ParseInput input
 
-    let differences = GetJoltDifferences adapters outlet device
+    let differences = GetJoltDifferences puzzleInput
 
-    let oneJoltdifferenceCount = differences |> Seq.filter ((=) 1L) |> Seq.length
-    let threeJoltdifferenceCount = differences |> Seq.filter ((=) 3L) |> Seq.length
+    let oneJoltDifferenceCount = differences |> Seq.filter ((=) 1L) |> Seq.length
+    let threeJoltDifferenceCount = differences |> Seq.filter ((=) 3L) |> Seq.length
 
-    oneJoltdifferenceCount * threeJoltdifferenceCount
+    oneJoltDifferenceCount * threeJoltDifferenceCount
+
+
+let CountAllArrangements puzzleInput =
+    let mutable arrangementCountCache = Map.empty
+
+    let rec countAllArrangements adapters currentJolts device =
+        match arrangementCountCache |> Map.tryFind currentJolts with
+        | Some cached -> cached
+        | None ->
+            match device - currentJolts with
+            | differenceWithDevice when differenceWithDevice <= 3L -> 1L
+            | _ ->
+                let possibleAdapters = adapters |> Seq.filter (fun a ->
+                    let difference = (a - currentJolts)
+                    difference >= 1L && difference <= 3L
+                )
+
+                let arrangementCount =
+                    possibleAdapters
+                    |> Seq.sumBy (fun adapter -> countAllArrangements adapters adapter device)
+
+                arrangementCountCache <-
+                    arrangementCountCache |> Map.change currentJolts (fun _ -> Some arrangementCount)
+
+                arrangementCount
+
+    countAllArrangements puzzleInput.Adapters puzzleInput.Outlet puzzleInput.Device
+
+let SolvePuzzlePartTwo input =
+    let puzzleInput = ParseInput input
+
+    CountAllArrangements puzzleInput
 
 
 [<EntryPoint>]
@@ -39,5 +77,6 @@ let main argv =
         |> List.ofArray
 
     printfn "Answer for part one is %d" (SolvePuzzle lines)
+    printfn "Answer for part two is %d" (SolvePuzzlePartTwo lines)
 
     0
--- 10-adapter-array/tests/Tests.fs
+++ 10-adapter-array/tests/Tests.fs
@@ -55,3 +55,57 @@ let ``SolvePuzzle WhenSecondExample ThenReturns22And10`` () =
 3".Replace("\r\n", "\n").Split("\n") |> List.ofSeq)
 
     Assert.Equal(22 * 10, result)
+
+[<Fact>]
+let ``SolvePuzzlePartTwo WhenFirstExample ThenReturns8`` () =
+    let result =
+        Program.SolvePuzzlePartTwo ("16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4".Replace("\r\n", "\n").Split("\n") |> List.ofSeq)
+
+    Assert.Equal(8L, result)
+
+[<Fact>]
+let ``SolvePuzzlePartTwo WhenSecondExample ThenReturns19208`` () =
+    let result =
+        Program.SolvePuzzlePartTwo ("28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3".Replace("\r\n", "\n").Split("\n") |> List.ofSeq)
+
+    Assert.Equal(19208L, result)
