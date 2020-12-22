92d9f3ac8816c735c6489ef46941587fafb5bc1b
Adam Gauthier <adamgauthier12@gmail.com>
2020-12-22 14:13:15 -0500

    Add solution for day 16 part two

--- 16-ticket-translation/src/Program.fs
+++ 16-ticket-translation/src/Program.fs
@@ -45,11 +45,12 @@ let ParseInput (input: string) =
 
     (fields, myTicket, nearbyTickets)
 
+let IsValidValueForField ticketValue field =
+    field.Ranges |> Seq.exists (fun range -> ticketValue >= range.Min && ticketValue <= range.Max)
 
-let IsInvalidValue fields ticketValue =
-    fields |> (not << Seq.exists (fun field ->
-        field.Ranges |> Seq.exists (fun range -> ticketValue >= range.Min && ticketValue <= range.Max)
-    ))
+let IsValidValue fields ticketValue =
+    fields
+    |> Seq.exists (IsValidValueForField ticketValue)
 
 let SolvePuzzle input =
     let (fields, _, nearbyTickets) = ParseInput input
@@ -58,10 +59,68 @@ let SolvePuzzle input =
         nearbyTickets
         |> Seq.collect (fun nearbyTicket ->
             nearbyTicket.Values
-            |> Seq.filter (IsInvalidValue fields)
+            |> Seq.filter (not << IsValidValue fields)
         )
 
     allInvalidValues |> Seq.sum
+
+
+let rec FindSolution (indexedFieldsWithoutSolution: list<int * list<Field>>) currentSolution =
+    if List.isEmpty indexedFieldsWithoutSolution then
+        currentSolution
+    else
+        let (indexesWithOneField, indexesWithMultipleFields) =
+            indexedFieldsWithoutSolution
+            |> List.partition (fun (_, possibleFields) -> List.length possibleFields = 1)
+
+        let indexesWithSolution = indexesWithOneField |> List.map (fun (index, fields) -> (index, fields |> List.exactlyOne))
+
+        let newIndexedFieldsLeft =
+            indexesWithMultipleFields
+            |> List.map (fun (index, fields) ->
+                let fieldsWithSolution = indexesWithSolution |> List.map snd
+                let newPossibleFields = fields |> List.except fieldsWithSolution
+
+                (index, newPossibleFields)
+            )
+
+        let newSolution = currentSolution @ indexesWithSolution
+
+        FindSolution newIndexedFieldsLeft newSolution
+
+
+let SolvePuzzlePartTwo input =
+    let (fields, myTicket, nearbyTickets) = ParseInput input
+
+    let validTickets = nearbyTickets |> List.filter (fun nearbyTicket ->
+        nearbyTicket.Values |> Seq.forall (IsValidValue fields)
+    )
+
+    let indexedPossibleFields =
+        [0..myTicket.Values.Length-1]
+        |> List.map (fun valueIndex ->
+            let ticketValuesForIndex =
+                validTickets
+                |> List.map (fun ticket -> ticket.Values.[valueIndex])
+
+            let possibleValidFields =
+                fields |> List.filter (fun field ->
+                    ticketValuesForIndex
+                    |> Seq.forall (fun value -> IsValidValueForField value field)
+                )
+
+            (valueIndex, possibleValidFields)
+        )
+
+    let solution = FindSolution indexedPossibleFields []
+
+    let departureFieldsMultiplied =
+        solution
+        |> List.filter (fun (_, field) -> field.Name.StartsWith("departure "))
+        |> List.map (fun (i, _) -> myTicket.Values.[i])
+        |> List.fold Checked.(*) 1L
+
+    departureFieldsMultiplied
 
 [<EntryPoint>]
 let main argv =
@@ -73,5 +132,6 @@ let main argv =
         |> String.concat "\n"
 
     printfn "Answer for part one is %d" (SolvePuzzle input)
+    printfn "Answer for part two is %d" (SolvePuzzlePartTwo input)
 
     0
